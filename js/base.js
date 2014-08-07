9c4eb68266d31d05c792bd998b9f6be70ca51073
Adam Pere <adampere@Adams-MacBook-Pro.local>
2014-08-07 16:16:09 -0400

    Changing possible values removes extra

--- js/base.js
+++ js/base.js
@@ -7,10 +7,11 @@ d3.chart('BlockChart', {
     this.rows = 1;
     this.pointSize = 10;
     this.updateScales();
-    this.pVals = ['low', 'medium', 'high', 'unknown'];
-    this.valCount = {'low' : 0, 'medium' : 0 , 'high' : 0, 'unknown' : 0};
+    this.pVals = ['low', 'medium', 'high', 'unknown']; // Possible Categories (Values)
+    this.valCount = {'low' : 0, 'medium' : 0 , 'high' : 0, 'unknown' : 0}; // Count of items that belong to each possible catgory
     $(this.base[0]).attr('height', this.h + 'px');
     $(this.base[0]).attr('width', this.w + 'px');
+    this.data = "";
     
     var tooltip = d3.select('body').append('div')
       .classed('tooltip', 'true')
@@ -28,13 +29,14 @@ d3.chart('BlockChart', {
         chart.rows = Math.ceil(data.length/chart.cols);
         chart.updateScales();
         data.sort(function(a,b) { var comparison =  chart.pVals.indexOf(a.value) - chart.pVals.indexOf(b.value); return comparison; });
+        chart.data = data;
         return this.selectAll('all-points').data(data); // return a data bound selection for the passed in data.
 
       },
       // setup the elements that were just created
       insert: function() {
         var chart = this.chart(); 
-        var returning = this.append('rect')  
+        var pointElements =  returning = this.append('rect')  
           .attr('class', function(d) {return 'category-' + chart.pVals.indexOf(d.value);})
           .attr('data-category', function(d) {return d.value;})
           .attr('data-category-index', function(d){return chart.pVals.indexOf(d.value);})
@@ -47,17 +49,18 @@ d3.chart('BlockChart', {
             for(var k = 0; k < chart.pVals.length; k++) {
               catPerc = catPerc + chart.valCount[chart.pVals[k]];
             }
-            catPerc = chart.valCount[d.value]/catPerc*100;
+            if(catPerc != 0) { catPerc = chart.valCount[d.value]/catPerc*100; }
+            
             tooltip.html('<span class = "tooltip-category">Category: ' + d.value + '</span><br><span class ="tooltip-category-num"># of items: ' + chart.valCount[d.value] + '</span><br><span class = "category-percentage">Percentage: ' + catPerc.toFixed(1) + '%</span>'); 
             tooltip.style("visibility", "visible");
-            d3.selectAll('.category-' + chart.pVals.indexOf(d.value)).attr("stroke", "black").attr("stroke-width", "10px").attr("stroke-opacity", .2);
+            d3.selectAll('.category-' + chart.pVals.indexOf(d.value)).attr("stroke", "black").attr("stroke-width", "10px").attr("stroke-opacity", .1);
             })
 	      .on("mousemove", function(){return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
 	      .on("mouseout", function(d){
             d3.selectAll('.category-' + chart.pVals.indexOf(d.value)).attr("stroke", "").attr("stroke-width", "").attr("stroke-opacity", "");
             return tooltip.style("visibility", "hidden");});
         
-        return returning;
+        return pointElements;
       },
 
       // setup an enter event for the data as it comes in:
@@ -66,6 +69,11 @@ d3.chart('BlockChart', {
           var chart = this.chart();        
           // position newly entering elements
           return this.attr('x', function(d,i) { return chart.getXCoordinate(d,i); });
+        },
+        'exit' : function() {
+          var chart = this.chart();        
+          // position newly entering elements
+          return this.remove();
         }
       }
     });
@@ -106,35 +114,49 @@ d3.chart('BlockChart', {
     }
     this.pointSize = newSize;
     this.updateScales();
-    this.updatePoints();
+    this.updateThePoints();
     return this;
   },
   possibleValues: function(values) { // sets the categories (aka possible data values). Takes in an array of values e.g ['low', 'medium', 'high', 'unkown'], items will be sorted by 
     if (arguments.length === 0) {
-      return this.pVals;
+      return chart.pVals;
     }
-    this.pVals = values;
-    return this;
+    chart.pVals = values;
+    chart.valCount = {};
+    
+    for(var k = 0; k < chart.pVals.length; k++) { // resetting the value of all
+      chart.valCount[chart.pVals[k]] = 0;
+    }
+    d3.selectAll('.all-points rect').each(function(d,i) { 
+      if(chart.pVals.indexOf(d.value) >= 0) {
+        chart.valCount[d.value] = chart.valCount[d.value] + 1;
+      }
+      else {
+        this.remove();
+      }
+    });
+    chart.updateThePoints();
+  },
+  valueCount: function() { // getter for the object that holds the count of all items by value
+    return this.valCount;
   },
   updateScales: function () { // resets the scales used for drawing the points
     this.xScale = d3.scale.linear().domain([1, this.cols]).range([0 + this.margin['left'], this.w - this.margin['right'] - this.pointSize]);
     this.yScale = d3.scale.linear().domain([1, this.rows]).range([0 + this.margin['top'], this.h  - this.margin['bottom'] - this.pointSize]);
   },
-  updatePoints: function() { // Redraws all of the points
-      d3.selectAll('.all-points rect')
+  updateThePoints: function() { // Redraws all of the points
+      chart.layer('all-points').selectAll('rect')
           .attr('y', function(d,i) {  return chart.getYCoordinate(d,i); } )
           .attr('x', function(d,i) { return chart.getXCoordinate(d,i); })
           .attr('width', chart.pointSize + 'px')
           .attr('height', chart.pointSize + 'px');
+
   },
+  
   getXCoordinate: function(d, i) { // returns the x-coordinate for a point   *** currently set for simply making 1 graphpoint per datapoint
-    var offset = 0;
-    if(i == 0) {offset = .0000000000000000001;}
-    return this.xScale(i - ((Math.ceil(((i+1)/this.cols) + offset)-1)*this.cols) + 1) + 'px'; // x = index - ((column-1) * (# of columns))
+    return this.xScale(i - ((Math.ceil(((i+1)/this.cols))-1)*this.cols) + 1) + 'px'; // x = index - ((column-1) * (# of columns))
   },
   getYCoordinate: function(d, i) { // returns the y-coordinate for a point  *** currently set for simply making 1 graphpoint per datapoint
-    var offset = 0;
-    if(i == 0) {offset = .0000000000000000001;}
-    return this.yScale(Math.ceil(((i+1)/this.cols) + offset)) + 'px'; // y = index / (# of rows)
+    return this.yScale(Math.ceil(((i+1)/this.cols))) + 'px'; // y = index / (# of rows)
   }
 });
